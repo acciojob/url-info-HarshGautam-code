556f768896ae30c59ba70f4a76104c5c71d92da8
HarshGautam-code <149058537+HarshGautam-code@users.noreply.github.com>
2026-02-26 18:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
 //your JS code here. If required.
-const url = window.location.href;
-const length = url.length;
-alert("The length of the URL is:" + length);
+alert("The length of the URL is: " + location.href.length);
